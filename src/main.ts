b6cdca1e7a6c1cb45f031a7b218a13bf81a77133
nick shoup <shoupnb@gmail.com>
2023-11-10 16:06:10 -0700

    adding .env and dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WebsocketsGateway } from './websockets/websocket.gateway';
-// import { IoClientAdapter } from './websockets/ioclientadapter.client';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config(); // Load environment variables from .env file
   const app = await NestFactory.create(AppModule);
   const websocketsGateway = app.get(WebsocketsGateway);
   websocketsGateway.afterInit(app.getHttpServer());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
--- src/websockets/websocket.gateway.ts
+++ src/websockets/websocket.gateway.ts
@@ -1,15 +1,16 @@
 import { OnModuleInit } from '@nestjs/common';
 import {
   WebSocketGateway,
-  WebSocketServer,
-  SubscribeMessage,
   OnGatewayInit,
   OnGatewayConnection,
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { Socket } from 'socket.io';
 import * as WebSocket from 'ws';
-import { generatePrivateKey, getPublicKey } from 'nostr-tools';
+import { v4 as uuidv4 } from 'uuid';
+
+const validWebSocketUriRegex =
+  /^wss?:\/\/(www\.)?[\w-]+(\.[a-z]{2,}){1,2}(:\d{1,5})?\/?$/;
 
 @WebSocketGateway()
 export class WebsocketsGateway
@@ -22,56 +23,66 @@ export class WebsocketsGateway
   afterInit(server: any) {
     console.log('server', server);
   }
-  @WebSocketServer() server: Server | undefined;
 
   private externalSockets: WebSocket[] = [];
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   onModuleInit() {
     console.log('WebSocket Gateway initialized');
-
-    // Initiate connections to external WebSocket servers
     this.initiateExternalConnections();
   }
 
   initiateExternalConnections() {
-    const server1 = new WebSocket('wss://nostr-pub.wellorder.net/');
-    this.externalSockets.push(server1);
+    if (process.env.RELAYWS) {
+      //I want to split here and then loop through the array and create a new websocket for each one
+      const relays = process.env.RELAYWS.split(',').map((relay) =>
+        relay.trim(),
+      );
+      relays.forEach((relay) => {
+        if (validWebSocketUriRegex.test(relay)) {
+          this.externalSockets.push(new WebSocket(relay));
+        } else {
+          console.error('Invalid WebSocket URI');
+        }
+      });
+    }
+
+    console.log('externalSockets', this.externalSockets);
+    console.log('process.env.PUBLIC_KEY', process.env.PUBLIC_KEY);
     this.externalSockets.forEach((socket, index) => {
-      try {
-        socket.on('open', () => {
-          console.log(`Connection to external server ${index + 1} opened`);
+      socket.on('open', () => {
+        console.log(`Connection to external server ${index + 1} opened`);
 
-          // Send the initial message to the external server
-          //this is the correct format for creating subscription to p tags in messages for user hex public key
-          const initialMessage = [
-            'REQ',
-            '98507d66-9fd0-433c-a2b5-1f9d0d0c361f',
-            {
-              kinds: [1],
-              '#p': [
-                '56d5de36eb4fed1e2fe99bfbfdea10ab5fa630a13c59d2e3c70dbb5b3988a572',
-              ],
-            },
-          ];
+        const initialMessage = [
+          'REQ',
+          this.getNewGuid(),
+          {
+            since: this.getLastOnline(),
+            kinds: [1, 6],
+            '#p': [process.env.PUBLIC_KEY],
+          },
+        ];
 
-          socket.send(JSON.stringify(initialMessage));
-        });
-      } catch (error) {
-        console.log(error);
-      }
+        socket.send(JSON.stringify(initialMessage));
+      });
 
       socket.on('message', (data: any) => {
-        this.handleExternalMessage(index, data);
+        this.handleEvents(index, data);
       });
 
       socket.on('close', () => {
         console.log(`Connection to external server ${index + 1} closed`);
       });
+
+      socket.on('error', (error) => {
+        console.error(
+          `Error in connection to external server ${index + 1}:`,
+          error,
+        );
+      });
     });
   }
 
-  handleExternalMessage(index: number, data: string) {
+  handleEvents(index: number, data: string) {
     try {
       const message = JSON.parse(data);
 
@@ -90,18 +101,22 @@ export class WebsocketsGateway
     }
   }
 
-  handleConnection(client: Socket, ...args: any[]) {
+  handleConnection(socket: Socket, ...args: any[]) {
     console.log(args);
-    console.log(`Client connected: ${client.id}`);
+    console.log(`Client connected: ${socket.id}`);
   }
 
-  handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
+  handleDisconnect(socket: Socket) {
+    console.log(`Client disconnected: ${socket.id}`);
   }
 
-  @SubscribeMessage('response')
-  handleResponse(client: Socket, data: any): void {
-    // Handle the response from the client
-    console.log('Response received:', data);
+  //need a function to get the last time the user was online
+  getLastOnline() {
+    //returns a unix timestamp number like 1630000000
+    return 1699542039;
+  }
+
+  getNewGuid() {
+    return uuidv4();
   }
 }
